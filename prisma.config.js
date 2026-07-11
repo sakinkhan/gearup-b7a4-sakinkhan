31b63e647b195e73a89ec77d945e3ed415d5cf15
sakinkhan <sakin2044@gmail.com>
2026-07-12 02:33:36 +1000

    schemas added

--- prisma.config.js
+++ prisma.config.js
@@ -1,12 +1,12 @@
 import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
 export default defineConfig({
-    schema: "prisma/schema.prisma",
-    migrations: {
-        path: "prisma/migrations",
-    },
-    datasource: {
-        url: env("DATABASE_URL"),
-    },
+  schema: "prisma/schemas",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  datasource: {
+    url: env("DATABASE_URL"),
+  },
 });
 //# sourceMappingURL=prisma.config.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,19 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, Request, Response, urlencoded } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import config from "./config";
 
 const app: Application = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: config.app_url,
+    credentials: true,
+  }),
+);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
